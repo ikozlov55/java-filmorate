f2d711a888ed499766abf7c2c478af2f97a7b4a5
ikozlov55 <ilya621@yandex.ru>
2025-05-25 19:30:37 +0300

    style fix

--- src/test/java/ru/yandex/practicum/filmorate/controller/GenresControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/GenresControllerTest.java
@@ -1,13 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.testdata.FilmorateApi;
 
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
